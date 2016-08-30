9688c6d19b267f4412fabab615bf3ae35827eb67
Kylan Hurt <kylan@datagoniaweb.com>
2016-08-29 21:31:59 -0700

    Apparent success initially setting up criterion table, but still need to set up default behavior, specifically with respect to non-registered users

---
+++ api/app/Criteria.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class criteria extends Model
+{
+    protected $table = 'criterion';
+    
+}
--- api/database/migrations/2016_08_28_004012_create_reviews_table.php
+++ api/database/migrations/2016_08_28_004012_create_reviews_table.php
@@ -12,21 +12,34 @@ class CreateReviewsTable extends Migration
      */
     public function up()
     {
+        Schema::create('criterion', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('user_id')->unsigned();
+            
+            //need one column for user id
+            $table->foreign('user_id')->references('id')->on('users');         
+            
+            $table->timestamps();
+        });     
+        
         Schema::create('reviews', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('score');
             $table->string('description')->nullable();
-            $table->string('criteria')->default('general');
             $table->integer('entity_id')->unsigned();
             $table->integer('user_id')->unsigned();
+            $table->integer('criteria_id')->unsigned()->nullable();
             
             //need one column for user id
             $table->foreign('user_id')->references('id')->on('users');
             //need one column for entity ID
             $table->foreign('entity_id')->references('id')->on('entities');
+            //need one more column, for criteria id
+            $table->foreign('criteria_id')->references('id')->on('criterion');
             
             //make user-entity-criteria a unique combo
-            $table->unique(array('entity_id','user_id','criteria'));
+            $table->unique(array('entity_id','user_id','criteria_id'));
             
             $table->timestamps();
         });
@@ -39,6 +52,7 @@ class CreateReviewsTable extends Migration
      */
     public function down()
     {
-        Schema::drop('reviews');
+        Schema::drop('reviews');            
+        Schema::drop('criterion');         
     }
 }
